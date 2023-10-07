a2a56f65f6a17d48d06cb2e975876efd69940db5
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2023-10-07 13:39:04 +0530

    added error_handler

--- services/blog/start.ts
+++ services/blog/start.ts
@@ -1,5 +1,7 @@
 import { startWebServer } from './entry-points/api/server';
 import { logger } from '@bloggo/logger';
+import { AppError, errorHandler } from '@bloggo/error-handling';
+
 
 async function start() {
     //Array of entry point is being used to support more entry-points kinds like message queue, scheduled job,
@@ -11,5 +13,7 @@ start()
         logger.info(`The app has started successfully ${JSON.stringify(startResponses)}}`);
     })
     .catch((error) => {
-        logger.error(`The app has failed to started: ${error}}`);
+        errorHandler.handleError(
+            new AppError('startup-failure', error.message, 500, false, error)
+          );
     });
